1b16d3d3b1e275689b880ffe9d918a30e9e73d1c
danicampora <danicampora@gmail.com>
2024-11-11 11:32:13 +0100

    Complete the module to communicate with the probe.

--- ports/zephyr/boards/manifest.py
+++ ports/zephyr/boards/manifest.py
@@ -1,12 +1,2 @@
 freeze("$(PORT_DIR)/modules")
-include("$(MPY_DIR)/extmod/asyncio")
-# Useful networking-related packages.
-#require("bundle-networking")
-# Require some micropython-lib modules.
-require("onewire")
-require("ds18x20")
-require("dht")
-require("neopixel")
-require("upysh")
-require("umqtt.robust")
-require("umqtt.simple")
+
--- ports/zephyr/modules/probe.py
+++ ports/zephyr/modules/probe.py
@@ -1,11 +1,29 @@
-import machine
+import binascii
 from machine import Pin, I2C
 import struct
 from micropython import const
 from probe_protocol import *
+import time
 
 
 PROBE_I2C_ADDR = const(0x7E)
+
+
+class Settings:
+
+    def __init__(self, cycle, trigger_from, trigger_to, operating_from, operating_to, trigger_delay, mode, trigger_window, sampling, min_max):
+        self.updated_at = 0
+        self.cycle = cycle
+        self.trigger_from = trigger_from
+        self.trigger_to = trigger_to
+        self.operating_from = operating_from
+        self.operating_to = operating_to
+        self.trigger_delay = trigger_delay
+        self.mode = mode
+        self.trigger_window = trigger_window
+        self.sampling = sampling
+        self.min_max = min_max
+        self.dummy = 0
 
 
 class Probe:
@@ -20,7 +38,34 @@ class Probe:
     def _create_request_header(self, request_type, payload_len):
         return struct.pack(PROTOCOL_REQUEST_HEADER_FORMAT, request_type, payload_len)
 
-    def _get_device_information(self):
+    def _perform_request(self, request_type, request_payload, response_payload_format, port):
+        _request_payload_len = 0
+        if request_payload is not None:
+            _request_payload_len = len(request_payload)
+        request = self._create_request_header(request_type, _request_payload_len)
+        _response_packed = self._data_transfer(request, struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT) + (struct.calcsize(response_payload_format) if response_payload_format is not None else 0))
+        _response_header = _response_packed[:struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT)]
+        _result, _response_payload_len = struct.unpack(PROTOCOL_RESPONSE_HEADER_FORMAT, _response_header)
+        _response_packed = _response_packed[struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT):]
+        return (True if _result == 0 else False), struct.unpack(response_payload_format, _response_packed)
+
+    def set_device_id(self, id, port):
+        id = binascii.unhexlify(id)
+        _unique_id_packed = struct.pack(PROTOCOL_REQUEST_SET_ID_FORMAT,
+                                        int(id[0]),
+                                        int(id[1]),
+                                        int(id[2]),
+                                        int(id[3]),
+                                        int(id[4]),
+                                        int(id[5]),
+                                        int(id[6]),
+                                        int(id[7]))
+        return self._perform_request(E_PROTOCOL_SET_UNIQUE_ID, _unique_id_packed, None, port)
+
+    def get_status(self, port):
+        return self._perform_request(E_PROTOCOL_GET_STATUS, None, PROTOCOL_RESPONSE_STATUS_FORMAT, port)
+
+    def get_device_information(self, port):
         request = self._create_request_header(E_PROTOCOL_GET_DEVICE_INFO, 0)
         _dev_info_packed = self._data_transfer(request, struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT) + struct.calcsize(PROTOCOL_RESPONSE_DEV_INFO_FORMAT))
         _dev_info_packed = _dev_info_packed[struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT):]
@@ -28,9 +73,47 @@ class Probe:
         print('Device ID is {:02X}{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}'.format(id_7, id_6, id_5, id_4, id_3, id_2, id_1, id_0)) 
         print('FW version is {}.{}.{}'.format(ver_0, ver_1, ver_2))
 
-    def _get_reading(self):
+    def get_reading(self, port):
         request = self._create_request_header(E_PROTOCOL_GET_READING, 0)
-        _dev_info_packed = self._data_transfer(request, struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT) + struct.calcsize(PROTOCOL_RESPONSE_READING_T1_FORMAT))
-        _dev_info_packed = _dev_info_packed[struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT):]
-        _temperature, _triggered = struct.unpack(PROTOCOL_RESPONSE_READING_T1_FORMAT, _dev_info_packed)
+        _reading_packed = self._data_transfer(request, struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT) + struct.calcsize(PROTOCOL_RESPONSE_READING_T1_FORMAT))
+        _reading_packed = _reading_packed[struct.calcsize(PROTOCOL_RESPONSE_HEADER_FORMAT):]
+        _temperature, _triggered = struct.unpack(PROTOCOL_RESPONSE_READING_T1_FORMAT, _reading_packed)
         print('T1 temperature value is: {:.2f} triggered: {}'.format(_temperature, True if _triggered else False))
+
+    def get_min_reading(self, port):
+        return self._perform_request(E_PROTOCOL_GET_MIN_READING, None, PROTOCOL_RESPONSE_MIN_MAX_READING_T1_FORMAT, port)
+
+    def get_max_reading(self, port):
+        return self._perform_request(E_PROTOCOL_GET_MAX_READING, None, PROTOCOL_RESPONSE_MIN_MAX_READING_T1_FORMAT, port)
+
+    def get_device_settings(self, port):
+        return self._perform_request(E_PROTOCOL_GET_SETTINGS, None, PROTOCOL_RESPONSE_SETTINGS_FORMAT, port)
+
+    def set_device_settings(self, settings, port):
+        settings.updated_at = time.time()
+        settings_packed = struct.pack(PROTOCOL_REQUEST_SETTINGS_FORMAT,
+                                      settings.updated_at,
+                                      settings.cycle,
+                                      settings.trigger_from,
+                                      settings.trigger_to,
+                                      settings.operating_from,
+                                      settings.operating_to,
+                                      settings.trigger_delay,
+                                      settings.mode,
+                                      settings.trigger_window,
+                                      settings.sampling,
+                                      settings.min_max,
+                                      settings.dummy)
+        return self._perform_request(E_PROTOCOL_UPDATE_SETTINGS, settings_packed, None, port)
+
+    def ping(self, port):
+        return self.get_status(port)
+
+    def sleep(self, port):
+        return self._perform_request(E_PROTOCOL_GO_TO_SLEEP, None, None, port)
+
+    def wake(self, port):
+        pass
+
+    def scan_triggers(self, port):
+        pass
